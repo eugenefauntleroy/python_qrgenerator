d60d5bc2ff8a97ba2db5b05bdf2950dbe898f447
Mich <51951486+eugenefauntleroy@users.noreply.github.com>
2020-11-30 20:27:10 -0800

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -2,6 +2,6 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 
-QRstring = "https://en.wikipedia.org/wiki/Independent_Order_of_Odd_Fellows"
+QRstring = "" #enter url here
 url = pyqrcode.create(QRstring)
 url.png("Desktop\\qr.png", scale=8)
